4eeae5d871e0f5f2ad6f44e91bd6bf21c928cd46
Peter Li <peter.s.li93@gmail.com>
2018-07-05 16:04:06 -0400

    -

--- VAE_multipie/AE_triplet.py
+++ VAE_multipie/AE_triplet.py
@@ -282,11 +282,11 @@ class AE(nn.Module):
 
 	def get_latent_vectors(self, x):
 		z = self.encode(x.view(-1, self.nc, self.ndf, self.ngf)) # whole latent vector
-		print("pre slice" z.size())
+		print("pre slice {}".format(z.size())
 		#z = z.view(1, 128)
 		z_per = z[0:64] # part of z repesenenting identity of the person
 		z_exp = z[64:]  # part of z representing the expression
-		print("post slice" z.size())
+		print("post slice {}".format(z.size()))
 		return z, z_per, z_exp
 
 	def forward(self, x):
