a0aef771aacf8e24bfd7cd1b7ed8ff6756ab21b0
Peter Li <peter.s.li93@gmail.com>
2018-07-06 09:55:58 -0400

    a

--- VAE_multipie/AE_pie.py
+++ VAE_multipie/AE_pie.py
@@ -225,10 +225,10 @@ class AE(nn.Module):
 		self.e5 = nn.Conv2d(ndf*8, ndf*8, 4, 2, 1)
 		self.bn5 = nn.BatchNorm2d(ndf*8)
 
-		self.fc1 = nn.Linear(ndf*8*2*4*4, latent_variable_size) #if ndf=64, args are (8192, 128)
+		self.fc1 = nn.Linear(ndf*8*4*4, latent_variable_size) #if ndf=64, args are (8192, 128)
 
 		# decoder
-		self.d1 = nn.Linear(latent_variable_size, ngf*8*2*4*4)
+		self.d1 = nn.Linear(latent_variable_size, ngf*8*4*4)
 
 		self.up1 = nn.Upsample(scale_factor=2)
 		self.pd1 = nn.ReplicationPad2d(1)
