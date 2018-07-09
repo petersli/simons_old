1f0c12a0868bcb99dbf76b4bb79ecbc344f5ade2
Peter Li <peter.s.li93@gmail.com>
2018-07-09 17:49:32 -0400

    fix test

--- VAE_multipie/AE_triplet.py
+++ VAE_multipie/AE_triplet.py
@@ -439,14 +439,14 @@ def test(epoch):
 
 		# test disentangling
 
-		z_per0_exp9 = torch.cat(z_per_dp0, z_exp_dp9, dim=1) # should be person 0 with expression 9
+		z_per0_exp9 = torch.cat((z_per_dp0, z_exp_dp9), dim=1) # should be person 0 with expression 9
 		recon_per0_exp9 = decode(z_per0_exp9)
 
 		visualizeAsImages(recon_per0_exp9.data.clone(), 
 		opt.dirImageoutput, 
 		filename='epoch_'+str(epoch)+'_per0_exp9', n_sample = 25, nrow=5, normalize=False)
 
-		z_per0_exp1 = torch.cat(z_per_dp0, z_exp_dp1, dim=1) # should look the same as dp0_img (exp1 and exp0 are the same)
+		z_per0_exp1 = torch.cat((z_per_dp0, z_exp_dp1), dim=1) # should look the same as dp0_img (exp1 and exp0 are the same)
 		recon_per0_exp1 = decode(z_per0_exp1)
 
 		visualizeAsImages(recon_per0_exp1.data.clone(), 
@@ -488,6 +488,7 @@ def start_training():
 
 	for epoch in range(start_epoch + 1, start_epoch + opt.epoch_iter + 1):
 		recon_loss, siamese_loss = train(epoch)
+		test(epoch)
 		torch.save(model.state_dict(),
 		 opt.dirCheckpoints + '/Epoch_{}_Recon_{:.4f}_Siamese_{:.4f}.pth'.format(epoch, recon_loss, siamese_loss))
 		if epoch % 10 == 0:
