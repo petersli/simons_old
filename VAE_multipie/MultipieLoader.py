7debdb79483e65be4d2c675e62362eedcc60581c
Peter Li <peter.s.li93@gmail.com>
2018-07-12 14:40:37 -0400

    p

--- VAE_multipie/MultipieLoader.py
+++ VAE_multipie/MultipieLoader.py
@@ -160,6 +160,7 @@ class FareMultipieExpressionTripletsFrontal(data.Dataset):
 
                 ids, ide, idp, idl = self.parse_imgfilename_fare_multipie(str(img0)[-20:-4])
                 key = (ids, ide)
+                print(str(img0))
                 print(key)
                 coords = dict[key]
                 img0 = img0[coords[1]:coords[3], coords[0]:coords[2]] #crop
@@ -173,6 +174,7 @@ class FareMultipieExpressionTripletsFrontal(data.Dataset):
 
                 ids, ide, idp, idl = self.parse_imgfilename_fare_multipie(str(img9)[-20:-4])
                 key = (ids, ide)
+                print(str(img9))
                 print(key)
                 coords = dict[key]
                 img9 = img9[coords[1]:coords[3], coords[0]:coords[2]] #crop
@@ -186,6 +188,7 @@ class FareMultipieExpressionTripletsFrontal(data.Dataset):
 
                 ids, ide, idp, idl = self.parse_imgfilename_fare_multipie(str(img1)[-20:-4])
                 key = (ids, ide)
+                print(str(img1))
                 print(key)
                 coords = dict[key]
                 img1 = img1[coords[1]:coords[3], coords[0]:coords[2]] #crop
